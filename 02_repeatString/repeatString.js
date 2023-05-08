75e8f18d7c8a78d1c23cfdaf91de0b3923b5c75e
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-08 11:55:25 -0300

    Complete first and second exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+function repeatString(string, repetition) {
+  i = 0
+  newString = ""
 
+  if (repetition < 0) {
+    return "ERROR"
+  }
+
+  while (i < repetition) {
+    newString = newString.concat(string)
+    i++
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
