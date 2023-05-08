3d7456c96dca7d1ddd2d35f464fdace9a2f7c3d7
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-08 17:16:22 -0300

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+  if((first < 0 || second < 0) || (typeof(first) != "number" || typeof(second) != "number")){
+    return "ERROR"
+  }
 
+  let min = Math.min(first, second)
+  let max = Math.max(first, second)
+  
+  let length = max - min
+  let i=0
+  let a = min
+  let b = min + 1
+
+  while (i<length) {
+    a = a + b
+    b = b + 1
+
+    i++
+  }
+
+  return a
 };
 
 // Do not edit below this line
