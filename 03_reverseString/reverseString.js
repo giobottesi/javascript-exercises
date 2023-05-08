88a96494dd9012cd5bf0624b34aa54cd1cc20eb4
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-08 15:51:54 -0300

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+function reverseString (string) {
+  splitArray = string.split("")
+  reversedArray = splitArray.reverse()
+  reversedString = reversedArray.join("")
 
+  return reversedString;
 };
 
 // Do not edit below this line
