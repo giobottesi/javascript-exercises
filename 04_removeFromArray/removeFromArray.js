27fd152e34b9788e754d13f6bad16d051d398e58
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-08 16:49:48 -0300

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = []
+  let i = 0
 
+  while (i < array.length) {
+    if (![args].flat().includes(array[i])) {
+      newArray.push(array[i])
+    }
+    i++
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
