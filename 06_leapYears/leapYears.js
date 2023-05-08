fe7765574466e31093c5e0efa386806c7bf4bbd6
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-08 17:29:11 -0300

    Complete sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  function divisibleByFour (year) {
+    return year % 4 === 0
+  }
+
+  function divisibleByHundred (year) {
+    return year % 100 === 0
+  }
+
+  function divisibleByFourHundred (year) {
+    return year % 400 === 0
+  }
+
+  return ((divisibleByFour(year) && !divisibleByHundred(year)) || divisibleByFourHundred(year))
 };
 
 // Do not edit below this line
