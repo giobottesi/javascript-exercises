fc7cf2ea355ac79c4e19ff8c29d0984f4eb2a7df
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-09 17:36:32 -0300

    Complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0
+
+  let sum = 0
+	array.forEach(element => sum = sum + element)
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let multiply = 1
+	array.forEach(element => multiply = multiply * element)
 
+  return multiply
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1
+  i = 1
+  while (i<=a) {
+    factorial = factorial * i
+    i++
+  }
+
+  return factorial
 };
 
 // Do not edit below this line
