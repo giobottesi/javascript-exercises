32b2d3be3ecacf6a37ab51ffa1533b6c88b057b7
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-09 18:04:37 -0300

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+  if (a < 0) return "OOPS"
+  let n = parseInt(a)
+  i = 1
+  let fibonacci = 1
+  let array = [1]
+
+  while (i < (n - 1)) {
+    array.push(fibonacci)
+    fibonacci = fibonacci + array[array.length - 2]
+    i++
+  }
+
+  return fibonacci
 
 };
 
