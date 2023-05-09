8854a3f5feb2511d410f0a491ecf3ef449883ce7
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-09 18:43:25 -0300

    Complete twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  getAge = people.map((person) => {
+    person.age = ((person.yearOfDeath != undefined ? person.yearOfDeath : (new Date().getFullYear())) - person.yearOfBirth)
+    return person
+  })
 
+  sortedPeople = getAge.sort((a, b) => (a.age > b.age) ? -1 : 1)
+
+  return sortedPeople[0]
 };
 
 // Do not edit below this line
